6604b6e374cfcf50332d3d3de9924addc5c28534
andres06 <andressierrarojas7@gmail.com>
2022-05-18 21:37:25 -0500

    Se agrega para buscar por id de personage en API

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { Request, Response } from 'express';
 //Para obtener datos de una api, como un "fech"
 import axios from 'axios';
 //MODULO PARA MEDIR EL TIEMPO DE RESPUESTA
@@ -13,7 +14,7 @@ app.use(responseTime());
 app.use(express.json());
 
 //////////////////////////////////////////////////////////////
-
+// Dredenciales o datos para la coneccion a Redis
 const data = {
 	//Redis of port -> 6379
 	port : 6000,
@@ -21,9 +22,10 @@ const data = {
 	password : "0606"
 } as RedisClientOptions;
 
-//Connection Redis
+//Creamos el cliente de la conección
 const client = createClient(data); //CReates a new client
 
+//Connection Redis
 
 client.on('connect', () =>{
 	console.log('Connected');
@@ -35,12 +37,13 @@ client.connect().then( res => {
 	console.error("Error during REDIS client connection", err);
 });
 
-app.get('/character', async (req, res) => {
+//Api
+app.get('/character', async (req : Request, res : Response) => {
 
 	
 	//Antes de todo se busca en redis
 	const getValueApiRickAndMorty = await client.get('characters');
-	//Validamos que si haya datos
+	//Validamos que si hay datos que concuerden
 	if(getValueApiRickAndMorty){
 		//Si hay lo mostramos
 		console.log('Parseando datos a "JSON"...');
@@ -50,7 +53,8 @@ app.get('/character', async (req, res) => {
 		return res.json(valueParse);
 	}
 	
-	//
+	//Si no existen los datos 
+	//Consultamos api y obtenemos los datos
 	const response = await axios.get("https://rickandmortyapi.com/api/character")
 
 	/**
@@ -62,21 +66,52 @@ app.get('/character', async (req, res) => {
 	 */
 
 	// console.log(response.data);
-
-
+	//Asingamos la Key y el value para Redis
 	const redisRes = await client.set("characters", JSON.stringify(response.data));
 	const age = await client.set('age',19);
 	//Respouesta
 	console.log('Redis Response : ',redisRes);
 	
-	
+	//Obtenemos el valor
 	const getValueApi = await client.get('characters')
 
 	console.log('Response get API : ', getValueApi);
-	
-	res.json(getValueApi);
+	//respondemos
+	return res.json(getValueApi);
 	
 })
+//Obtener datos por ID
+app.get('/character/:id', async ( req : Request, res : Response) => {
+
+	//Obtenemos el parametro de ruta
+	const { id } = req.params;
+	console.log('ID : ', id);
+	
+	console.log('-> ',req.originalUrl);
+	
+	try {
+		//Buscamos si existe los datos
+		const reply = await client.get(req.originalUrl);
+		//Validamos
+		console.log('** ',reply);
+		if (reply) return res.json(JSON.stringify(reply));
+
+		//Si no existe los datos
+		//Hcemos la peticion a la API
+		const response = await axios.get('https://rickandmortyapi.com/api/character/' + id)
+
+		console.log(response.data);
+
+		const saveDataApi = await client.set(id, JSON.stringify(response.data));
+		
+		return res.json(response.data);
+		
+	} catch (err) {
+		console.log(err);
+		
+	}
+
+});
 
 app.listen(3000)
 console.log('Server on port 3000');
